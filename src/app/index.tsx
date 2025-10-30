e724f1feaa87b67c7433fc380651bb916c7cd4d9
Duclaux Tagne <tagneduclaux@gmail.com>
2025-10-30 14:10:02 +0100

    Init app and setup expo-router

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function index() {
+  return (
+    <View>
+      <Text>this is my index home page</Text>
+    </View>
+  )
+}
